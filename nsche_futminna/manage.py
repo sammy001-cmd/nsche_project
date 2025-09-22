efddbb19d55be23aa51a0a0b82c5f4c32184ef52
sammy <samueltoluwani169@gmail.com>
2025-09-22 11:53:28 +0100

    nsche project

--- nsche_futminna/manage.py
+++ nsche_futminna/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsche_futminna.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsche_futminna.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nsche_futminna.nsche_futminna.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
