c1e8742dae94a37871b784a5137cf444d1f10084
sammy <samueltoluwani169@gmail.com>
2025-09-22 14:24:34 +0100

    newie

--- nsche_futminna/nsche_futminna/settings.py
+++ nsche_futminna/nsche_futminna/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--)*j4!le$q_nj3#y^x+ji%@$b4(+=x)wg9bp!b21ot565xp83s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["nschefutminna.pythonanywhere.com"]
 
 
 # Application definition
@@ -86,11 +86,7 @@ WSGI_APPLICATION = "nsche_futminna.wsgi.application"
 # }
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": dj_database_url.config(default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 }
 
 TEMPLATES = [
@@ -149,9 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
